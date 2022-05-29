a36c104b55d8bc3be8c93f01e3f60cdced28e1ae
Rana omar <80989305+ranasin19@users.noreply.github.com>
2022-05-29 19:18:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn00.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn00.Location = new System.Drawing.Point(25, 40);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(87, 78);
+            this.btn00.TabIndex = 0;
+            this.btn00.Text = "button1";
+            this.btn00.UseVisualStyleBackColor = false;
+            // 
+            // btn22
+            // 
+            this.btn22.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn22.Location = new System.Drawing.Point(211, 208);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(87, 78);
+            this.btn22.TabIndex = 1;
+            this.btn22.Text = "button2";
+            this.btn22.UseVisualStyleBackColor = false;
+            // 
+            // btn20
+            // 
+            this.btn20.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn20.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn20.Location = new System.Drawing.Point(25, 208);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(87, 78);
+            this.btn20.TabIndex = 2;
+            this.btn20.Text = "button3";
+            this.btn20.UseVisualStyleBackColor = false;
+            // 
+            // btn02
+            // 
+            this.btn02.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn02.Location = new System.Drawing.Point(211, 40);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(87, 78);
+            this.btn02.TabIndex = 3;
+            this.btn02.Text = "button4";
+            this.btn02.UseVisualStyleBackColor = false;
+            // 
+            // btn11
+            // 
+            this.btn11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn11.Location = new System.Drawing.Point(118, 124);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(87, 78);
+            this.btn11.TabIndex = 4;
+            this.btn11.Text = "button5";
+            this.btn11.UseVisualStyleBackColor = false;
+            // 
+            // btn01
+            // 
+            this.btn01.BackColor = System.Drawing.Color.White;
+            this.btn01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn01.Location = new System.Drawing.Point(118, 40);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(87, 78);
+            this.btn01.TabIndex = 5;
+            this.btn01.Text = "button6";
+            this.btn01.UseVisualStyleBackColor = false;
+            // 
+            // btn10
+            // 
+            this.btn10.BackColor = System.Drawing.Color.White;
+            this.btn10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn10.Location = new System.Drawing.Point(25, 124);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(87, 78);
+            this.btn10.TabIndex = 6;
+            this.btn10.Text = "button7";
+            this.btn10.UseVisualStyleBackColor = false;
+            // 
+            // btn21
+            // 
+            this.btn21.BackColor = System.Drawing.Color.White;
+            this.btn21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn21.Location = new System.Drawing.Point(118, 208);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(87, 78);
+            this.btn21.TabIndex = 7;
+            this.btn21.Text = "button8";
+            this.btn21.UseVisualStyleBackColor = false;
+            // 
+            // btn12
+            // 
+            this.btn12.BackColor = System.Drawing.Color.White;
+            this.btn12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn12.Location = new System.Drawing.Point(211, 124);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(87, 78);
+            this.btn12.TabIndex = 8;
+            this.btn12.Text = "button9";
+            this.btn12.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 450);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        enum Gametype { X, o };
+        Gametype current_gt = Gametype.X;
+        int playd_games = 0;
+        Button[,] btns = new Button[3, 3];
+        public Form1()
+        {
+            InitializeComponent();
+            btns[0, 0] = btn00; btns[0, 1] = btn01; btns[0, 2] = btn02;
+            btns[1, 0] = btn10; btns[1, 1] = btn11; btns[1, 2] = btn12;
+            btns[2, 0] = btn20; btns[2, 1] = btn21; btns[2, 2] = btn22;
+            foreach (Button btn in btns) btn.Click += new EventHandler(btn_click);
+
+        }
+
+        void btn_click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Text != "") return;
+            playd_games++;
+            btn.Text = current_gt.ToString();
+            if (IsWin())
+            {
+                MessageBox.Show("مبروك" + current_gt.ToString() + "لقد فزت");
+                NewGame();
+                return;
+            }
+            if (playd_games == 9)
+            {
+                MessageBox.Show("النتيجه تعادل");
+                NewGame();
+            }
+            switchGame();
+        }
+        void NewGame()
+        {
+            foreach (Button btn in btns) btn.Text = "";
+            playd_games = 0;
+        }
+        void switchGame()
+        {
+            if (current_gt == Gametype.o) current_gt = Gametype.o;
+            else current_gt = Gametype.X;
+        }
+
+        bool IsWin()
+        {
+            for (int i = 0; 1 <= 2; i++)
+            {
+                if (btns[0, i].Text == current_gt.ToString() &&
+                    btns[0, i].Text == btns[1, i].Text &&
+                    btns[i, 1].Text == btns[2, i].Text)
+                    return true;
+
+                if (btns[i, 0].Text == current_gt.ToString() &&
+                   btns[i, i].Text == btns[i, 1].Text &&
+                   btns[i, 1].Text == btns[i, 2].Text)
+                    return true;
+            }
+            if (btns[0, 0].Text == current_gt.ToString() &&
+               btns[0, 0].Text == btns[1, 1].Text &&
+                    btns[1, 1].Text == btns[2, 2].Text)
+                return true;
+
+            if (btns[0, 2].Text == current_gt.ToString() &&
+               btns[0, 2].Text == btns[1, 1].Text &&
+               btns[1, 1].Text == btns[2, 0].Text)
+                return true;
+            return false;
+        }
+    }
+ }
